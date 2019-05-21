7b7c23bc7bb06ca60bf85bb80a1f09eea3e16927
Sebastian de Bellefon <arnaudpourseb@gmail.com>
2019-05-21 08:44:30 -0400

    bench

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,56 @@
 module Main where
 
---import           Data.Time.Clock.POSIX (getPOSIXTime)
--- import Types
+import           Foreign.C.Types                 (CInt, CChar)
+import           Data.Vector.Storable            (Vector)
+import qualified Data.Vector.Storable            as V
+import           Data.Monoid                     ((<>))
+import           Data.Time.Clock.POSIX (getPOSIXTime)
+import Types
+import Lib
+
+inputDataSample0 :: Vector CChar
+inputDataSample0 = V.fromList [a,a,a,a,c,g,a] <> V.fromList (take 93 (repeat a))
+
+inputDataSample1 :: Vector CChar
+inputDataSample1 = V.fromList [c,g,a,a,a,a,a] <> V.fromList (take 93 (repeat a)) 
+
+inputDataSample2 :: Vector CChar
+inputDataSample2 = V.fromList [a,a,a,a,a,a,a] <> V.fromList (take 93 (repeat a))
+
+pattern_CG, pattern_cCGA, pattern_CGA, pattern_cccccccccc :: [Pweight]
+pattern_CG = [Pweight 0 1 0 0, Pweight 0 0 1 0]
+pattern_cCGA = [Pweight 0 0.1 0 0, Pweight 0 1 0 0, Pweight 0 0 1 0, Pweight 0.5 0 0 0]
+pattern_CGA = [Pweight 0 1 0 0, Pweight 0 0 1 0]
+pattern_cccccccccc = [cPattern, cPattern, cPattern, cPattern, cPattern, cPattern, cPattern, cPattern, cPattern, cPattern]
+  where cPattern = Pweight 0 0 0.001 0
+
+bench :: IO ()
+bench = do
+    matches <- findPatternsInBlock (mkNucleotideAndPositionBlock inputData) (mkPatterns patterns)
+    print (expected == matches)
+  where numberOfPeople = 10000 :: Int
+
+        inputData :: [(Vector CChar, Vector CInt)]
+        inputData =  [(inputDataSample0, inputDataPositions), (inputDataSample1, inputDataPositions)] ++ (take (numberOfPeople - 2) (repeat (inputDataSample2, inputDataPositions)))
+
+        inputDataPositions :: Vector CInt
+        inputDataPositions = V.fromList $ take (V.length inputDataSample0) [0..]
+
+        patterns = [pattern_CG, pattern_cCGA, pattern_CGA] ++ replicate 50 pattern_cccccccccc ++ [pattern_CG]
+
+        expected = [Match {mPatternId = 53, mScore = 1000, mPosition = 0, mSampleId = 1, mMatched = [c, g]}
+                    ,Match {mPatternId = 2,  mScore = 1000, mPosition = 0, mSampleId = 1, mMatched = [c, g]}
+                    ,Match {mPatternId = 0,  mScore = 1000, mPosition = 0, mSampleId = 1, mMatched = [c, g]}
+                    ,Match {mPatternId = 53, mScore = 1000, mPosition = 4, mSampleId = 0, mMatched = [c, g]}
+                    ,Match {mPatternId = 2,  mScore = 1000, mPosition = 4, mSampleId = 0, mMatched = [c, g]}
+                    ,Match {mPatternId = 0,  mScore = 1000, mPosition = 4, mSampleId = 0, mMatched = [c, g]}
+                    ,Match {mPatternId = 1,  mScore = 961,  mPosition = 3, mSampleId = 0, mMatched = [a, c, g, a]}]
 
 main :: IO()
 main = do
     print "Hello world"
-    --t1 <- getPOSIXTime
-    --a <- runBlock numberOfPeople block_size inputData patternData
-    --t2 <- getPOSIXTime
-    --print "Time in ms"
-    --print $ round $ (t2 - t1) * 1000
-    --print a
-    --print (a == [Match {mPatternId = 53, mScore = 1000, mPosition = 0, mSampleId = 1},Match {mPatternId = 2, mScore = 1000, mPosition = 0, mSampleId = 1},Match {mPatternId = 0, mScore = 1000, mPosition = 0, mSampleId = 1},Match {mPatternId = 53, mScore = 1000, mPosition = 4, mSampleId = 0},Match {mPatternId = 2, mScore = 1000, mPosition = 4, mSampleId = 0},Match {mPatternId = 0, mScore = 1000, mPosition = 4, mSampleId = 0},Match {mPatternId = 1, mScore = 961, mPosition = 3, mSampleId = 0}])
+    t1 <- getPOSIXTime
+    bench
+    t2 <- getPOSIXTime
+    print (round $ (t2 - t1) * 1000 :: Integer) -- milliseconds
+    pure ()
--- test/Main.hs
+++ test/Main.hs
@@ -11,13 +11,13 @@ import Types
 import Lib
 
 inputDataSample0 :: Vector CChar
-inputDataSample0 = V.fromList [a,a,a,a,c,g,a] <> V.fromList (take 93 (repeat a))
+inputDataSample0 = V.fromList [a,a,a,a,c,g,a] <> V.fromList (replicate 93 a)
 
 inputDataSample1 :: Vector CChar
-inputDataSample1 = V.fromList [c,g,a,a,a,a,a] <> V.fromList (take 93 (repeat a)) 
+inputDataSample1 = V.fromList [c,g,a,a,a,a,a] <> V.fromList (replicate 93 a) 
 
 inputDataSample2 :: Vector CChar
-inputDataSample2 = V.fromList [a,a,a,a,a,a,a] <> V.fromList (take 93 (repeat a))
+inputDataSample2 = V.fromList [a,a,a,a,a,a,a] <> V.fromList (replicate 93 a)
 
 pattern_CG, pattern_cCGA, pattern_CGA, pattern_cccccccccc :: [Pweight]
 pattern_CG = [Pweight 0 1 0 0, Pweight 0 0 1 0]
@@ -30,9 +30,7 @@ test_patterns_basic :: IO ()
 test_patterns_basic = do
     matches <- findPatternsInBlock (mkNucleotideAndPositionBlock inputData) (mkPatterns patterns)
     assertEqual expected matches
-  where numberOfPeople = 1000 :: Int
-
-        inputData :: [(Vector CChar, Vector CInt)]
+  where inputData :: [(Vector CChar, Vector CInt)]
         inputData =  [(inputDataSample0, inputDataPositions)]
 
         inputDataPositions :: Vector CInt
@@ -46,9 +44,7 @@ test_patterns_basic2 :: IO ()
 test_patterns_basic2 = do
     matches <- findPatternsInBlock (mkNucleotideAndPositionBlock inputData) (mkPatterns patterns)
     assertEqual expected matches
-  where numberOfPeople = 1000 :: Int
-
-        inputData :: [(Vector CChar, Vector CInt)]
+  where inputData :: [(Vector CChar, Vector CInt)]
         inputData =  [(inputDataSample0, inputDataPositions)]
 
         inputDataPositions :: Vector CInt
@@ -66,12 +62,12 @@ test_patterns_1 = do
   where numberOfPeople = 1000 :: Int
 
         inputData :: [(Vector CChar, Vector CInt)]
-        inputData =  [(inputDataSample0, inputDataPositions), (inputDataSample1, inputDataPositions)] ++ (take (numberOfPeople - 2) (repeat (inputDataSample2, inputDataPositions)))
+        inputData =  [(inputDataSample0, inputDataPositions), (inputDataSample1, inputDataPositions)] ++ replicate (numberOfPeople - 2) (inputDataSample2, inputDataPositions)
 
         inputDataPositions :: Vector CInt
         inputDataPositions = V.fromList $ take (V.length inputDataSample0) [0..]
 
-        patterns = [pattern_CG, pattern_cCGA, pattern_CGA] ++ take 50 (repeat pattern_cccccccccc) ++ [pattern_CG]
+        patterns = [pattern_CG, pattern_cCGA, pattern_CGA] ++ replicate 50 pattern_cccccccccc ++ [pattern_CG]
 
         expected = [Match {mPatternId = 53, mScore = 1000, mPosition = 0, mSampleId = 1, mMatched = [c, g]}
                    ,Match {mPatternId = 2,  mScore = 1000, mPosition = 0, mSampleId = 1, mMatched = [c, g]}
@@ -89,12 +85,12 @@ test_patterns_2 = do
   where numberOfPeople = 1000 :: Int
 
         inputData :: [(Vector CChar, Vector CInt)]
-        inputData =  [(inputDataSample0, inputDataPositions), (inputDataSample1, V.map (+7) inputDataPositions)] ++ (take (numberOfPeople - 2) (repeat (inputDataSample2, inputDataPositions)))
+        inputData =  [(inputDataSample0, inputDataPositions), (inputDataSample1, V.map (+7) inputDataPositions)] ++ replicate (numberOfPeople - 2) (inputDataSample2, inputDataPositions)
 
         inputDataPositions :: Vector CInt
         inputDataPositions = V.fromList $ take (V.length inputDataSample0) [10..]
 
-        patterns = [pattern_CG, pattern_cCGA, pattern_CGA] ++ take 50 (repeat pattern_cccccccccc) ++ [pattern_CG]
+        patterns = [pattern_CG, pattern_cCGA, pattern_CGA] ++ replicate 50 pattern_cccccccccc ++ [pattern_CG]
 
         expected = [Match {mPatternId = 53, mScore = 1000, mPosition = 10 + 7, mSampleId = 1, mMatched = [c, g]}
                    ,Match {mPatternId = 2,  mScore = 1000, mPosition = 10 + 7, mSampleId = 1, mMatched = [c, g]}
@@ -117,7 +113,7 @@ test_patterns_padding = do
         inputDataPositions :: Vector CInt
         inputDataPositions = V.fromList $ take (V.length inputDataSample0) [10..]
 
-        patterns = [pattern_CG, pattern_cCGA, pattern_CGA] ++ take 50 (repeat pattern_cccccccccc) ++ [pattern_CG]
+        patterns = [pattern_CG, pattern_cCGA, pattern_CGA] ++ replicate 50 pattern_cccccccccc ++ [pattern_CG]
 
         expected = [Match{mPatternId = 53, mScore = 1000, mPosition = 10, mSampleId = 1, mMatched = [c, g]},
                     Match{mPatternId = 2,  mScore = 1000, mPosition = 10, mSampleId = 1, mMatched = [c, g]},
@@ -131,4 +127,5 @@ test_patterns_padding = do
 ----prop_reverse xs = xs == (myReverse (myReverse xs))
 
 main :: IO ()
-main = htfMain htf_thisModulesTests
+main = 
+  htfMain htf_thisModulesTests
